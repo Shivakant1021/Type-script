2c5ec49adbb1a1b166c7744dd33b28a48082fa29
Shivakant1021 <shivakant011999@gmail.com>
2025-07-10 01:12:36 +0530

    First Commit

---
+++ Lacture1/app.js
@@ -0,0 +1,7 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var n1 = 10;
+var n2 = 30;
+var ans = n1 + n2;
+console.log(ans);
---
+++ Lacture1/app.ts
@@ -0,0 +1,9 @@
+function add(num1 : number, num2: number){
+    return num1 + num2;
+}
+
+const n1 = 10;
+const n2 = 30;
+const ans = n1 + n2;
+
+console.log(ans);
